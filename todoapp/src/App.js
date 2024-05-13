7499a032050a45fdb3fe31e1a17cab72d9a5037c
Kaito Umemoto <kaito.umemoto.uhd@gmail.com>
2024-05-13 23:39:36 +0900

    タスクの追加機能　（Todo入力して追加ボタンをクリック後、未完了のTODOリストにタスクが追加）

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 
 export const App = () => {
+  //   TODO入力欄の値を取得するため、入力部分をstateとして定義
+  const [todoText, setTodoText] = useState("");
   // 未完了TODOを定義
   // TODOは複数作成できるようにするため、配列[]で定義
   const [incompleteTodos, setIncompleteTodos] = useState([
@@ -11,11 +13,33 @@ export const App = () => {
   // 完了TODOを定義
   // TODOは複数作成できるようにするため、配列[]で定義
   const [completeTodos, setCompleteTodos] = useState(["タスク3"]);
+  // 関数の引数(event)には、入力された情報が格納されており、event.target.valueとすることで入力された値を取得できる
+  // 取得した値をsetTodoTextの引数に渡すことで、todoTextのstateに反映させている。
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    // if文で入力欄が空の場合、処理を実行しないよう制御している。
+    if (todoText === "") return;
+    // newTodos変数には、今現在表示されているタスクと新たに追加したタスクを結合した新たな配列が格納されている
+    // スプレッド構文(...)を使用して今現在表示されているタスクを格納
+    // todoTextには、新たに追加したタスクが格納されている。
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    // setTodoText("");とすることで、追加後に入力欄をリセットしている。
+    setTodoText("");
+  };
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          // inputのvalue属性に{todoText}を設定することで、入力された値が表示されるようにしている
+          value={todoText}
+          // onChangeイベントを使用して入力された値を取得し、入力欄に反映させる処理を関数に定義
+          onChange={onChangeTodoText}
+        />
+        {/* onClickイベントを使用し、追加ボタン押下後の処理を定義した関数を設定 */}
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
