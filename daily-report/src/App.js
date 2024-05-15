4dae1377ea2162d2beb23efd6c5cd89ae71db078
Kaito Umemoto <kaito.umemoto.uhd@gmail.com>
2024-05-15 15:18:52 +0900

    日報入力画面の完成

--- daily-report/src/App.js
+++ daily-report/src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState({
+    date: "",
+    title: "",
+    content: "",
+  });
+  const [errors, setErrors] = useState({});
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [e.target.name]: "",
+    }));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    const newErrors = {};
+    if (!formData.date) {
+      newErrors.date = "日時を設定してください";
+    }
+
+    if (!formData.title) {
+      newErrors.title = "タイトルを入力してください";
+    }
+
+    if (!formData.content) {
+      newErrors.content = "内容が入っていません";
+    }
+
+    if (Object.keys(newErrors).length === 0) {
+      console.log("データを送信しました");
+      setFormData({ date: "", title: "", content: "" });
+      setErrors({});
+    } else {
+      setErrors(newErrors);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="heading">日報</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label className="label">日時：</label>
+          {errors.name && <span className="error">{errors.name}</span>}
+          <input
+            type="date"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="input"
+          />
+        </div>
+        <div className="form-field">
+          <label className="label">タイトル：</label>
+          {errors.title && <span className="error">{errors.title}</span>}
+          <input
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            className="input"
+          />
+        </div>
+        <div className="form-field">
+          <label className="label">内容：</label>
+          {errors.content && <span className="error">{errors.content}</span>}
+          <textarea
+            type="text"
+            name="content"
+            value={formData.content}
+            onChange={handleChange}
+            className="input"
+          />
+        </div>
+        <button type="submit" className="button">
+          送信
+        </button>
+      </form>
     </div>
   );
 }
